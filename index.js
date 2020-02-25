fbcf37c9bf264544f662d8abc38116b276466b92
João Salomão <joaosalomao.613@gmail.com>
2020-02-25 20:39:56 -0300

    Melhorando randomização

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ client.on("ready", () => {
 client.on("message", msg => {
   if (msg.author.bot) return;
 
-  if (
-    msg.content.toLocaleLowerCase() === "sh!cat"
-  ) {
-    sendCatPicture(msg)
+  if (msg.content.toLocaleLowerCase() === "sh!cat") {
+    sendCatPicture(msg);
   }
 });
 
@@ -20,11 +18,11 @@ client.login("NjgxOTYxODY3NzE5MzQ0MTgx.XlWFAQ.Iql3XKv-zgLmscqmfCafMWGSuWA");
 
 async function sendCatPicture(message) {
   const response = await axios.get(
-    "https://pixabay.com/api/?key=15390583-f0d2e1b4c8c96b27978676e4e&q=cats&image_type=photo"
+    `https://pixabay.com/api/?key=15390583-f0d2e1b4c8c96b27978676e4e&q=gato&image_type=photo&per_page=200`
   );
-  const url =
-    response.data.hits[Math.floor(Math.random() * response.data.hits.length)]
-      .largeImageURL;
+  
+  const index = Math.floor(Math.random() * response.data.hits.length);
+  const url = response.data.hits[index].largeImageURL;
 
   message.reply("a cute cat for you", {
     files: [url]
