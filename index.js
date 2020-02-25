42f4847867883d75979a60f884b2c711c69f63e2
João Salomão <joaosalomao.613@gmail.com>
2020-02-25 20:10:58 -0300

    Definindo comando para chamar o bot

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("message", msg => {
   if (msg.author.bot) return;
 
   if (
-    msg.content.toLocaleLowerCase().includes("<@&681963238602178591> gatinho")
+    msg.content.toLocaleLowerCase() === "sh!cat"
   ) {
     sendCatPicture(msg)
   }
